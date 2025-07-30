065524f52d8f877e9d405aba24f6e3714a89851e
Ronaldo Almeida <ranto@pobox.com>
2025-07-30 17:23:46 -0300

    Primeiro Commit

---
+++ nixos/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix with Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+  	# create a nixOS-system module i call 'nixos' (hostname)
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      
+      # define system architecture, so packages know how to install
+      system = "x86_64-linux";
+      
+      # let the modules include my inputs using the builtin 'specialArgs' attribute
+      specialArgs = {inherit inputs;};
+      modules = [
+      	# Point to a Nix file containing my system configuration
+        ./my-nixos-config/configuration.nix
+      ];
+    };
+  };
+}
+
